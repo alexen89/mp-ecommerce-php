dfd5044a23ff373a46355ba0976286159228a56e
Alejandro Enciso <aencisoj@everis.com>
2020-11-04 09:32:17 -0600

    integration notification, prefrence configuration

--- checkout.php
+++ checkout.php
@@ -1,7 +1,7 @@
 <?php
 // SDK
 require __DIR__ .  '/vendor/autoload.php';
-$baseUrl = $_SERVER['SERVER_ADDR'];
+$baseUrl = $_SERVER['SERVER_NAME'];
 //credentiales
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
